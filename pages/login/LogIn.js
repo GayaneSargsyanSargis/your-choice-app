4957fca9bb5e05d0d7384df40450f1d085598ff8
Hasmik-Hohannisyan <82596963+Hasmik-Hohannisyan@users.noreply.github.com>
2021-12-27 13:49:50 +0400

    Add LogIn and SignUp pages.

--- pages/login/LogIn.js
+++ pages/login/LogIn.js
@@ -1,51 +1,21 @@
-﻿////window.addEventListener('load', async () => {
-////    const token = window.localStorage.getItem('token');
-////    if (token) {
-////        window.location.href = '/home.html';
-////    }
-////    document.getElementById('login').addEventListener('click', async () => {
-////        const errorLabel = document.getElementById('error-label');
-////        errorLabel.innerText = '';
-////        const username = document.querySelector('input[name="username"]').value;
-////        const password = document.querySelector('input[name="password"]').value;
-
-////        login(username, password).then(response => {
-////            if (response.success === true) {
-////                window.localStorage.setItem('token', response.data);
-////                window.location.href = '/home.html';
-////            } else {
-////                errorLabel.innerText = response.message;
-////            }
-////        });
-////    });
-////});
-
-function validateEmail() {
+﻿function validateEmail() {
     var form = document.getElementById("form");
     var email = document.getElementById("email").value;
     var text = document.getElementById("text1");
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
     if (email.match(pattern)) {
-        form.classList.add("valid");
-        form.classList.remove("Invalid");
         text.innerHTML = "Your Email Address in Valid."
         text.style.color = "#228B22";
     }
     else {
-        form.classList.remove("valid");
-        form.classList.add("Invalid");
         text.innerHTML = "Please Enter Valid Email Address."
         text.style.color = "#FF0000";
     }
 
     if (email == "") {
-        form.classList.remove("valid");
-        form.classList.remove("Invalid");
         text.innerHTML = ""
-        text.style.color = "##228B22";
     }
-
 
 
     var password = document.getElementById("psw");
@@ -53,48 +23,87 @@ function validateEmail() {
     var capital = document.getElementById("capital");
     var number = document.getElementById("number");
     var length = document.getElementById("length");
+    var validCount = 0;
 
     password.onfocus = function () {
-        document.getElementById("message").style.display = "block";
+        document.getElementById("message2").style.display = "block";
     }
 
     password.onblur = function () {
-        document.getElementById("message").style.display = "none";
+        document.getElementById("message2").style.display = "none";
+
+
+        var lowerCaseLetters = /[a-z]/g;
+        if (password.value.match(lowerCaseLetters)) {
+            letter.classList.remove("invalid");
+            letter.classList.add("valid");
+            validCount++;
+        } else {
+            letter.classList.remove("valid");
+            letter.classList.add("invalid");
+            validCount--;
+        }
+
+        var upperCaseLetters = /[A-Z]/g;
+        if (password.value.match(upperCaseLetters)) {
+            capital.classList.remove("invalid");
+            capital.classList.add("valid");
+            validCount++;
+        } else {
+            capital.classList.remove("valid");
+            capital.classList.add("invalid");
+            validCount--;
+        }
+
+        var numbers = /[0-9]/g;
+        if (password.value.match(numbers)) {
+            number.classList.remove("invalid");
+            number.classList.add("valid");
+            validCount++;
+        } else {
+            number.classList.remove("valid");
+            number.classList.add("invalid");
+            validCount--;
+        }
+
+        if (password.value.length >= 6 && password.value.length <= 20) {
+            length.classList.remove("invalid");
+            length.classList.add("valid");
+            validCount++;
+        } else {
+            length.classList.remove("valid");
+            length.classList.add("invalid");
+            validCount--;
+        }
+
+        if (validCount == 4) {
+            document.getElementById("message2").style.display = "none";
+
+        }
     }
 
-    var lowerCaseLetters = /[a-z]/g;
-    if (password.value.match(lowerCaseLetters)) {
-        letter.classList.remove("invalid");
-        letter.classList.add("valid");
-    } else {
-        letter.classList.remove("valid");
-        letter.classList.add("invalid");
+}
+function validAll() {
+    var login = document.getElementById("login");
+
+    login.onfocus = function () {
+        document.getElementById("message1").style.display = "block";
     }
 
-    var upperCaseLetters = /[A-Z]/g;
-    if (password.value.match(upperCaseLetters)) {
-        capital.classList.remove("invalid");
-        capital.classList.add("valid");
-    } else {
-        capital.classList.remove("valid");
-        capital.classList.add("invalid");
+    login.onblur = function () {
+        document.getElementById("message1").style.display = "none";
     }
 
-    var numbers = /[0-9]/g;
-    if (password.value.match(numbers)) {
-        number.classList.remove("invalid");
-        number.classList.add("valid");
-    } else {
-        number.classList.remove("valid");
-        number.classList.add("invalid");
-    }
+    login.addEventListener('click', function () {
 
-    if (password.value.length >= 6 && password.value.length <= 20) {
-        length.classList.remove("invalid");
-        length.classList.add("valid");
-    } else {
-        length.classList.remove("valid");
-        length.classList.add("invalid");
-    }
-    
+        if (email.length == 0 || password.length == 0) {
+            login.classList.remove("invalid");
+            login.classList.add("valid");
+        }
+        else {
+            login.classList.remove("invalid");
+            login.classList.remove("valid");
+        }
+    });
+
 }
--- pages/sign-up/SignUp.js
+++ pages/sign-up/SignUp.js
@@ -1,78 +1,69 @@
-////window.addEventListener('load', async () => {
-////    const token = window.localStorage.getItem('token');
-////    if (token) {
-////        window.location.href = '/home.html';
-////    }
-////    document.getElementById('login').addEventListener('click', async () => {
-////        const errorLabel = document.getElementById('error-label');
-////        errorLabel.innerText = '';
-////        const username = document.querySelector('input[name="username"]').value;
-////        const password = document.querySelector('input[name="password"]').value;
-
-////        login(username, password).then(response => {
-////            if (response.success === true) {
-////                window.localStorage.setItem('token', response.data);
-////                window.location.href = '/home.html';
-////            } else {
-////                errorLabel.innerText = response.message;
-////            }
-////        });
-////    });
-////});
-
 function validateEmail() {
-
+    var firstName = document.getElementById("firstName");
+    var lastName = document.getElementById("lastName");
     var password = document.getElementById("psw");
     var letter = document.getElementById("letter");
     var capital = document.getElementById("capital");
     var number = document.getElementById("number");
     var length = document.getElementById("length");
+    var validCount = 0;
 
     password.onfocus = function () {
-        document.getElementById("message").style.display = "block";
+        document.getElementById("message2").style.display = "block";
     }
 
     password.onblur = function () {
-        document.getElementById("message").style.display = "none";
+        document.getElementById("message2").style.display = "none";
+
+
+        var lowerCaseLetters = /[a-z]/g;
+        if (password.value.match(lowerCaseLetters)) {
+            letter.classList.remove("invalid");
+            letter.classList.add("valid");
+            validCount++;
+        } else {
+            letter.classList.remove("valid");
+            letter.classList.add("invalid");
+            validCount--;
+        }
+
+        var upperCaseLetters = /[A-Z]/g;
+        if (password.value.match(upperCaseLetters)) {
+            capital.classList.remove("invalid");
+            capital.classList.add("valid");
+            validCount++;
+        } else {
+            capital.classList.remove("valid");
+            capital.classList.add("invalid");
+            validCount--;
+        }
+
+        var numbers = /[0-9]/g;
+        if (password.value.match(numbers)) {
+            number.classList.remove("invalid");
+            number.classList.add("valid");
+            validCount++;
+        } else {
+            number.classList.remove("valid");
+            number.classList.add("invalid");
+            validCount--;
+        }
+
+        if (password.value.length >= 6 && password.value.length <= 20) {
+            length.classList.remove("invalid");
+            length.classList.add("valid");
+            validCount++;
+        } else {
+            length.classList.remove("valid");
+            length.classList.add("invalid");
+            validCount--;
+        }
+
+        if (validCount == 4) {
+            document.getElementById("message2").style.display = "none";
+
+        }
     }
-
-    var lowerCaseLetters = /[a-z]/g;
-    if (password.value.match(lowerCaseLetters)) {
-        letter.classList.remove("invalid");
-        letter.classList.add("valid");
-    } else {
-        letter.classList.remove("valid");
-        letter.classList.add("invalid");
-    }
-
-    var upperCaseLetters = /[A-Z]/g;
-    if (password.value.match(upperCaseLetters)) {
-        capital.classList.remove("invalid");
-        capital.classList.add("valid");
-    } else {
-        capital.classList.remove("valid");
-        capital.classList.add("invalid");
-    }
-
-
-    var numbers = /[0-9]/g;
-    if (password.value.match(numbers)) {
-        number.classList.remove("invalid");
-        number.classList.add("valid");
-    } else {
-        number.classList.remove("valid");
-        number.classList.add("invalid");
-    }
-
-
-    if (password.value.length >= 6 && password.value.length <= 20) {
-        length.classList.remove("invalid");
-        length.classList.add("valid");
-    } else {
-        length.classList.remove("valid");
-        length.classList.add("invalid");
-    }
-
 
 
     var email = document.getElementById("email").value;
@@ -121,5 +112,27 @@ function validateEmail() {
         }
     }
 
+}
 
+function validAll() {
+    var signUp = document.getElementById("signUp");
+    signUp.onfocus = function () {
+        document.getElementById("message1").style.display = "block";
+    }
+
+    signUp.onblur = function () {
+        document.getElementById("message1").style.display = "none";
+    }
+    s
+    signUp.addEventListener('click', function () {
+
+        if (email == "" || password == "" || firstName == "" || lastName == "" || confirmPassword == "") {
+            signUp.classList.remove("invalid");
+            signUp.classList.add("valid");
+        }
+        else {
+            signUp.classList.remove("invalid");
+            signUp.classList.remove("valid");
+        }
+    });
 }
